1f3ae01b9fa654dff15ee43c34e700aa8fe62e36
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-07-26 18:23:51 +0530

    Create my_cp_templete.cpp

---
+++ my_cp_templete.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,fma")
+#define Md_Nihal ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define mod9 1e9+7
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+#define int long long
+
+
+void solve() {
+    
+}
+
+
+signed   main()
+{
+    Md_Nihal;
+    int testcases = 1, i = 0;    cin >> testcases;
+    while ((i++) < testcases) {
+        //cout<<"case# : "<<testcases<<endl; 
+        solve() ; 
+        cout << endl; 
+    }
+}
