a16bea656798eb90fd1cbe29d61d2f206e4b16fd
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-08-06 10:14:25 +0530

    Update my_cp_templete.cpp

--- my_cp_templete.cpp
+++ my_cp_templete.cpp
@@ -1,23 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("O3,Ofast,unroll-loops")
-#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,fma")
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,lzcnt,mmx,abm,avx,avx2,fma")
 #define Md_Nihal ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define mod9 1e9+7
 #define endl "\n"
 #define all(x) x.begin(),x.end()
 #define int long long
 
+#ifndef ONLINE_JUDGE
+    #include "debug.h" 
+#else
+  #define debug(x...);
+#endif
 
 void solve() {
-
+    
 }
 
 
 signed   main()
 {
     Md_Nihal;
-    int testcases = 1, i = 0;    cin >> testcases;
+    int testcases = 1, i = 0;     cin >> testcases;
     while ((i++) < testcases) {
         /* cout<<"case# : "<<testcases<<endl; */ solve() ;
         cout << endl;
