338e9283374f2da51b6525f057200d84a0b9fdba
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-08-26 17:16:09 +0530

    initially it accepts 6 arguments but now changed to 10

--- debug.hpp
+++ debug.hpp
@@ -1,4 +1,3 @@
-// colout print
 #include<bits/stdc++.h>
 using namespace std;
 template <class T1, class T2>
@@ -15,15 +14,20 @@ ostream &operator<<(ostream &os, const T &c) {
   return os << ']';
 }
 
-#define _NTH_ARG(_1, _2, _3, _4, _5, _6, N, ...) N
+#define _NTH_ARG(_1, _2, _3, _4, _5, _6,_7,_8,_9,_10, N, ...) N
 #define _FE_1(_CALL, x) _CALL(x)
 #define _FE_2(_CALL, x, ...) _CALL(x) _FE_1(_CALL, __VA_ARGS__)
 #define _FE_3(_CALL, x, ...) _CALL(x) _FE_2(_CALL, __VA_ARGS__)
 #define _FE_4(_CALL, x, ...) _CALL(x) _FE_3(_CALL, __VA_ARGS__)
 #define _FE_5(_CALL, x, ...) _CALL(x) _FE_4(_CALL, __VA_ARGS__)
 #define _FE_6(_CALL, x, ...) _CALL(x) _FE_5(_CALL, __VA_ARGS__)
+#define _FE_7(_CALL, x, ...) _CALL(x) _FE_6(_CALL, __VA_ARGS__)
+#define _FE_8(_CALL, x, ...) _CALL(x) _FE_7(_CALL, __VA_ARGS__)
+#define _FE_9(_CALL, x, ...) _CALL(x) _FE_8(_CALL, __VA_ARGS__)
+#define _FE_10(_CALL, x, ...) _CALL(x) _FE_9(_CALL, __VA_ARGS__)
+
 #define FOR_EACH_MACRO(MACRO, ...)                                             \
-  _NTH_ARG(__VA_ARGS__, _FE_6, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1)              \
+  _NTH_ARG(__VA_ARGS__, _FE_10,_FE_9,_FE_8,_FE_7,_FE_6, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1)              \
   (MACRO, __VA_ARGS__)
 #define watch(x) cout << #x <<" = " << (x) << "\n";
 #define debug(...)                                                             \
