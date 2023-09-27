5dd2ffc551ca18f7519a3d68750f53d5ca9aed03
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-09-27 17:16:33 +0530

    Create Segtree_and.cpp

---
+++ Segtree_and.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,lzcnt,mmx,abm,avx,avx2,fma")
+#define Md_Nihal ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+
+typedef unsigned long long ull;
+#define endl "\n"
+#define int long long
+#define sz(a) (int)a.size()
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define vs vector<string>
+#define all(x) x.begin(),x.end()
+const int mod = 1e9 + 7;
+const int N = 1e6;
+const int inf = LONG_LONG_MAX;
+
+
+template<typename typC, typename typD> istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template<typename typC> istream &operator>>(istream &cin, vector<typC> &a) { for (auto &x : a) cin >> x; return cin; }
+template <class ...Args> auto &getm(Args &...args) { return (cin >> ... >> args); }
+#define get(...) __VA_ARGS__; getm(__VA_ARGS__)
+
+
+#ifndef ONLINE_JUDGE
+#include "/home/anonymous/nihal/others/CP/constest/debug.hpp"
+#else
+#define debug(x...);
+#endif
+vector<int> segtree;
+
+void build(int s, int e, int i, vector<int> &a)
+{
+    if (s == e)
+    {
+        segtree[i] = a[s];
+        return;
+    }
+    int mid = s + (e - s) / 2;
+    build(s, mid, 2 * i + 1, a);
+    build(mid + 1, e, 2 * i + 2, a);
+    segtree[i] = segtree[2 * i + 1] & segtree[2 * i + 2];
+}
+
+int range(int s, int e, int i, int qs, int qe)
+{
+    if (qs <= s && qe >= e)
+        return segtree[i];
+    if (qs > e || qe < s)
+        return INT_MAX;
+    int mid = s + (e - s) / 2;
+    int l = range(s, mid, 2 * i + 1, qs, qe);
+    int r = range(mid + 1, e, 2 * i + 2, qs, qe);
+    return l & r;
+}
+
+
+/*=========================================== SOLUTION ===========================================*/
+
+
+
+void solve() {
+
+    int n; cin >> n;
+    vi v(n); cin >> v;
+    int q; cin >> q;
+    segtree.resize(4 * n);
+    build(0, n - 1, 0, v);
+    for (int i = 0; i < q; ++i) {
+        int l, r; cin >> l >> r;
+        l--;
+        int low = l, high = n - 1, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int ok = range(0, n - 1, 0, l, mid);
+            if (ok >= r) {
+                low = mid + 1;
+                ans = mid;
+            } else high = mid - 1;
+        }
+        (ans >= 0) ? cout << ans + 1 << " " : cout << ans << " ";
+    }
+
+}
+
+
+
+/*============================================= MAIN =============================================*/
+
+signed   main()
+{
+    Md_Nihal;
+    int testcases = 1, i = 0;    cin >> testcases;
+    while ((i++) < testcases) {
+        solve() ;
+        cout << endl;
+    }
+}
