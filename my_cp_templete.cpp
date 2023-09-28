0a1b174067eb86fef3682d5f249aacc4dd6dc1c4
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-09-28 23:49:12 +0530

    Updated my_cp_templete.cpp

--- my_cp_templete.cpp
+++ my_cp_templete.cpp
@@ -1,9 +1,8 @@
-#include<bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,lzcnt,mmx,abm,avx,avx2,fma")
-#define Md_Nihal ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-
+#define Md_Nihal ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 typedef unsigned long long ull;
 #define endl "\n"
@@ -13,17 +12,18 @@ typedef unsigned long long ull;
 #define vpi vector<pair<int,int>>
 #define vs vector<string>
 #define all(x) x.begin(),x.end()
-const int mod = 1e9 + 7;
-const int N = 1e6;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define print(x) (x)?cout<<"YES":cout<<"NO";
+const int mod = 1e9 + 7, inf = LONG_LONG_MAX, N = 1e6;
 
+template<class T, class V>istream& operator>>(istream &in, pair<T, V> &a) {in >> a.first >> a.second; return in;}
+template<class T>istream& operator>>(istream &in, vector<T> &a) {for (auto &i : a) {in >> i;} return in;}
+template<class T, class V>ostream& operator<<(ostream &os, pair<T, V> &a) {os << a.first << " " << a.second; return os;}
+template<class T>ostream& operator<<(ostream &os, vector<T> &a) {for (int i = 0 ; i < sz(a) ; i++) {if (i != 0) {os << ' ';} os << a[i];} return os;}
 
-template<typename typC, typename typD> istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
-template<typename typC> istream &operator>>(istream &cin, vector<typC> &a) { for (auto &x : a) cin >> x; return cin; }
-template <class ...Args> auto &getm(Args &...args) { return (cin >> ... >> args); }
-#define get(...) __VA_ARGS__; getm(__VA_ARGS__)
 
 #ifndef ONLINE_JUDGE
-#include "/home/anonymous/nihal/others/CP/constest/debug.hpp"
+#include "debug.hpp"
 #else
 #define debug(x...);
 #endif
@@ -32,7 +32,9 @@ template <class ...Args> auto &getm(Args &...args) { return (cin >> ... >> args)
 
 
 
-void solve() {
+void __solve() {
+
+    
 
 }
 
@@ -40,12 +42,9 @@ void solve() {
 
 /*============================================= MAIN =============================================*/
 
-signed   main()
+signed  main()
 {
-	Md_Nihal;
-	int testcases = 1, i = 0;    cin >> testcases;
-	while ((i++) < testcases) {
-		solve() ;
-		cout << endl;
-	}
+    Md_Nihal;
+    int testcases = 1;    cin >> testcases;
+    for (int i = 1; i <= testcases; ++i, cout << endl) __solve();
 }
