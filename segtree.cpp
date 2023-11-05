ee3d510ce70e327f2638107fd46b00c94f88939f
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-11-05 18:57:01 +0530

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,41 +1,20 @@
-#include<bits/stdc++.h>
-#define int long long
-#define all(x) x.begin(),x.end()
-using namespace std;
-
-struct SegmentTree {
+class SegmentTree {
     typedef int T;
-    static constexpr T unit = 1;        //change this
-    T f(T a, T b) { return a * b / max(1LL, __gcd(a, b)); } // (any associative fn)
+    static constexpr T unit = 0;        //change this
+    T fun(T a, T b) { return a + b; } // (any associative fn)
     vector<T> s; int n;
+public:
     SegmentTree(int n = 0, T def = unit) : s(2 * n, def), n(n) {}
     void update(int pos, T val) {
         for (s[pos += n] = val; pos /= 2;)
-            s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
+            s[pos] = fun(s[pos * 2], s[pos * 2 + 1]);
     }
     T query(int b, int e) { // query [b, e)
         T ra = unit, rb = unit;
         for (b += n, e += n; b < e; b /= 2, e /= 2) {
-            if (b % 2) ra = f(ra, s[b++]);
-            if (e % 2) rb = f(s[--e], rb);
+            if (b % 2) ra = fun(ra, s[b++]);
+            if (e % 2) rb = fun(s[--e], rb);
         }
-        return f(ra, rb);
+        return fun(ra, rb);
     }
 };
-int32_t main()
-{
-    int T = 1; cin >> T;
-    while (T--) {
-
-        int n; cin >> n;
-        SegmentTree arr(n, 0);
-        for (int i = 0; i < n; i++) {
-            int x; cin >> x;
-            arr.update(i, x);
-        }
-        cout << arr.query(3, 5);
-
-        cout << endl;
-
-    }
-}
