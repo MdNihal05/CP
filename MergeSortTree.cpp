2c39af523946fba6bf7e475c8a22fa10d95667a5
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-11-21 12:40:38 +0530

    Update MergeSortTree.cpp

--- MergeSortTree.cpp
+++ MergeSortTree.cpp
@@ -7,9 +7,15 @@
 for r inclusive{
     change s+1==e to s==e in build 
     and range should be (l,mid) and (mid+1,r) in build and get_ans
+    if (s > r || e < l) {
+            return -1;
+    }
 }
 
 For count of elements
+if (s > r || e < l) {
+            return 0;
+}
 if (l <= s && e <= r) {
     return lower_bound(all(Tree[v]), rv) - lower_bound(all(Tree[v]), lv);
 }
