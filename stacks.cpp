b82aa4cda5ef59395706f54f6dd3ba4d88e79604
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-25 10:59:12 +0530

    Update stacks.cpp

--- stacks.cpp
+++ stacks.cpp
@@ -1,11 +1,18 @@
+vector<int>left(n, -1), right(n, -1);
+stack<int>stk;
 for (int i = 0; i < n; i++) {
-        cin >> v[i];
         while (stk.size() and v[i] > v[stk.top()]) {
             right[stk.top()] = i;
             stk.pop();
         }
-        if (stk.size()) {
-            left[i] = v[stk.top()];
+        stk.push(i);
+}
+while (stk.size())stk.pop();
+    for (int i = n - 1; i >= 0; i--) {
+        while (stk.size() and v[i] > v[stk.top()]) {
+            left[stk.top()] = i;
+            stk.pop();
         }
         stk.push(i);
 }
+debug(left, right);
