650328dba53e1e1264f7a20974ed4a1f6a9aec02
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-29 18:22:15 +0530

    Update my_cp_templete.cpp

--- my_cp_templete.cpp
+++ my_cp_templete.cpp
@@ -1,8 +1,15 @@
+// Author : md_nihal
+
 #include "bits/stdc++.h"
 using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,lzcnt,mmx,abm,avx,avx2,fma")
 #define Md_Nihal ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 
 typedef unsigned long long ull;
 #define endl "\n"
@@ -13,18 +20,31 @@ typedef unsigned long long ull;
 #define vpi vector<pair<int,int>>
 #define vs vector<string>
 #define all(x) x.begin(),x.end()
-#define rep(i,n) for (int i = 0; i < (n); i++)
-#define print(x) (x)?cout<<"YES":cout<<"NO";
-const int mod = 1e9 + 7, inf = LONG_LONG_MAX, N = 1e6+7;
+#define ALL(x) x.rbegin(),x.rend()
+#define rep(x,n) for (int i = x; i <=(n); i++)
+#define print(x) (x)?cout<<"YES\n":cout<<"NO\n";
+const int mod = 1e9 + 7, inf = LONG_LONG_MAX, N = 1e6 + 7;
 
 template<class T, class V>istream& operator>>(istream &in, pair<T, V> &a) {in >> a.first >> a.second; return in;}
 template<class T>istream& operator>>(istream &in, vector<T> &a) {for (auto &i : a) {in >> i;} return in;}
 template<class T, class V>ostream& operator<<(ostream &os, pair<T, V> &a) {os << a.first << " " << a.second; return os;}
 template<class T>ostream& operator<<(ostream &os, vector<T> &a) {for (auto &i : a) {os << i << " ";} return os;}
 
+/*----------------------------------------------- PBDS --------------------------------------------*/
+
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<class key, class value, class cmp = std::less<key>>
+using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+typedef gp_hash_table<int, int, hash<int>> ht;
+
+/*----------------------------------------------- PBDS ends --------------------------------------------*/
 
 #ifndef ONLINE_JUDGE
-#include "/home/anonymous/cpp_files/debug2.hpp"
+#include "debug.hpp"
 #else
 #define debug(x...);
 #endif
@@ -33,9 +53,9 @@ template<class T>ostream& operator<<(ostream &os, vector<T> &a) {for (auto &i : 
 
 
 
-void __solve() {
+void __solve(int testcases) {
 
-    
+
 
 }
 
@@ -47,5 +67,5 @@ int32_t  main()
 {
     Md_Nihal;
     int testcases = 1;    cin >> testcases;
-    for (int i = 1; i <= testcases; ++i, cout << endl) __solve();
+    for (int i = 0; i < testcases; cout << endl) __solve(++i);
 }
