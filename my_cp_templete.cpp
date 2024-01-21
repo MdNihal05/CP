0a96385d57f6323fcefda5b8b4842f8b953703f6
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2024-01-21 14:20:46 +0530

    Update my_cp_templete.cpp

--- my_cp_templete.cpp
+++ my_cp_templete.cpp
@@ -14,7 +14,8 @@ using namespace std;
 typedef unsigned long long ull;
 #define endl "\n"
 #define int long long
-#define Lint int64_t
+#define i_64 int64_t
+#define pi pair<int,int>
 #define sz(a) (int)a.size()
 #define vi vector<int>
 #define vpi vector<pair<int,int>>
@@ -23,7 +24,7 @@ typedef unsigned long long ull;
 #define ALL(x) x.rbegin(),x.rend()
 #define rep(x,n) for (int i = x; i <=(n); i++)
 #define print(x) (x)?cout<<"YES\n":cout<<"NO\n";
-const int mod = 1e9 + 7, inf = LONG_LONG_MAX, N = 1e6 + 7;
+const int mod = 1e9 + 7, inf = LONG_LONG_MAX, N = 1e6 + 7, MAX = 1e14 + 4;
 
 template<class T, class V>istream& operator>>(istream &in, pair<T, V> &a) {in >> a.first >> a.second; return in;}
 template<class T>istream& operator>>(istream &in, vector<T> &a) {for (auto &i : a) {in >> i;} return in;}
@@ -35,7 +36,7 @@ template<class T>ostream& operator<<(ostream &os, vector<T> &a) {for (auto &i : 
 using namespace __gnu_pbds;
 template<class T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
-template<class key, class value, class cmp = std::less<key>>
+template<class key, class value, class cmp = less<key>>
 using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
 // find_by_order(k)  returns iterator to kth element starting from 0;
 // order_of_key(k) returns count of elements strictly smaller than k;
