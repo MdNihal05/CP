70ce7b49bcf6974439bd3a20e96c830d4255e0a2
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2024-02-14 21:26:08 +0530

    Update seive.cpp

--- Templetes/seive.cpp
+++ Templetes/seive.cpp
@@ -1,12 +1,24 @@
-vector<int> primes(sp , 1);
-void sieve() {
-    primes[1] = 0;
-    primes[0] = 0;
-    for (ll i = 2; i * i <= sp; i++) {
-        if (primes[i] == 1) {
-            for (ll j = i * i; j <= sp; j += i) {
-                primes[j] = 0;
+bool marked[N];
+int  sp[N];
+
+void Sieve() {
+    for (int i = 2; i < N; i += 2)	sp[i] = 2;//even numbers have smallest prime factor 2
+    for (int i = 3; i < N; i += 2) {
+        if (!marked[i]) {
+            sp[i] = i;
+            for (int j = i; (j * i) < N; j += 2) {
+                if (!marked[j * i])	marked[j * i] = true, sp[j * i] = i;
             }
         }
     }
+}
+
+
+vector <int> factorize(int k) {
+    vector <int> ans;
+    while (k > 1) {
+        ans.push_back(sp[k]);
+        k /= sp[k];
+    }
+    return ans;
 }
