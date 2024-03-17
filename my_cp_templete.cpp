d7e540f92ec5ba1e0803828622854b9286354377
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2024-03-17 15:40:59 +0530

    Update my_cp_templete.cpp

--- my_cp_templete.cpp
+++ my_cp_templete.cpp
@@ -3,28 +3,21 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
 #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,lzcnt,mmx,abm,avx,avx2,fma")
 #define Md_Nihal ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 
 typedef unsigned long long ull;
 #define endl "\n"
 #define int long long
 #define i_64 int64_t
-#define pi pair<int,int>
 #define sz(a) (int)a.size()
 #define vi vector<int>
 #define pii pair<int,int>
 #define vpi vector<pii>
-#define vs vector<string>
 #define all(x) x.begin(),x.end()
 #define ALL(x) x.rbegin(),x.rend()
-#define rep(x,n) for (int i = x; i <=(n); i++)
-#define print(x) (x)?cout<<"YES\n":cout<<"NO\n";
+#define rep(x,n) for (int i = x; i <x+n; i++)
 const int mod = 1e9 + 7, inf = LONG_LONG_MAX, N = 1e6 + 7, MAX = 1e14 + 4;
 
 template<class T, class V>istream& operator>>(istream &in, pair<T, V> &a) {in >> a.first >> a.second; return in;}
@@ -33,6 +26,9 @@ template<class T, class V>ostream& operator<<(ostream &os, pair<T, V> &a) {os <<
 template<class T>ostream& operator<<(ostream &os, vector<T> &a) {for (auto &i : a) {os << i << " ";} return os;}
 
 /*----------------------------------------------- PBDS --------------------------------------------*/
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace __gnu_pbds;
 template<class T>
@@ -47,6 +43,7 @@ using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_nod
 #ifndef ONLINE_JUDGE
 #include "debug.hpp"
 #else
+#define DEBUG
 #define debug(x...);
 #endif
 
@@ -54,9 +51,7 @@ using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_nod
 
 
 
-void __solve(int testcases) {
-
-
+void __solve() {
 
 }
 
@@ -68,5 +63,5 @@ int32_t  main()
 {
     Md_Nihal;
     int testcases = 1;    cin >> testcases;
-    for (int i = 0; i < testcases; cout << endl) __solve(++i);
+    for (int i = 0; i < testcases; i++, __solve(), cout << endl);
 }
